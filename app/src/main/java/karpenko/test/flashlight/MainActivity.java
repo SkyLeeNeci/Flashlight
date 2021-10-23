05f2cfdbc1b6120607d577b3b750352196c46eda
SkyLeeNeci <92163391+SkyLeeNeci@users.noreply.github.com>
2021-10-23 14:20:43 +0300

    add Permission add flashlight function

--- app/src/main/java/karpenko/test/flashlight/MainActivity.java
+++ app/src/main/java/karpenko/test/flashlight/MainActivity.java
@@ -2,15 +2,78 @@ package karpenko.test.flashlight;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import dalvik.system.DexClassLoader;
 
 public class MainActivity extends AppCompatActivity {
 
-    
+    ImageButton imageButton;
+    boolean state;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageButton = findViewById(R.id.flashbtn);
+
+        Dexter.withContext(this).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                flashlight();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                Toast.makeText(MainActivity.this,"", Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+            }
+        }).check();
+    }
+
+    private void flashlight() {
+
+        imageButton.setOnClickListener(v ->{
+            if(!state){
+                CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+                try {
+                    String cameraId = cameraManager.getCameraIdList()[0];
+                    cameraManager.setTorchMode(cameraId,true);
+                    state = true;
+                    imageButton.setImageResource(R.drawable.torch_on);
+                }catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }else {
+                CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+                try {
+                    String cameraId = cameraManager.getCameraIdList()[0];
+                    cameraManager.setTorchMode(cameraId,false);
+                    state = false;
+                    imageButton.setImageResource(R.drawable.torch_off);
+                }catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
